062ddb5d28bb01972281c3d73ae64d4aeac6f09a
rgunczer <rgunczer@gmail.com>
2019-10-11 22:55:28 +0200

    express mod minimal

--- apps/mock-backend/src/main.ts
+++ apps/mock-backend/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to mockBackend!' });
+  res.json({ message: 'Welcome to mockBackendZ!' });
 });
 
 const port = process.env.port || 3333;
